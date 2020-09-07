813291bed9108cb36cc0f4ec8d9c09330fddb054
Thobio Joseph <jthobio2@gmail.com>
2020-09-06 23:18:27 -0700

    Initial Commit

---
+++ UI Challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UI Challenge
+//
+//  Created by Zerone Consulting on 06/09/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UI Challenge/UI_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  UI_ChallengeApp.swift
+//  UI Challenge
+//
+//  Created by Zerone Consulting on 06/09/20.
+//
+
+import SwiftUI
+
+@main
+struct UI_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
